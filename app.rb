14fe50cf17702af82b79169e518619e24e089da9
hogewest <hogewest@gmail.com>
2013-11-16 17:40:43 +0900

    リファクタリング

--- app.rb
+++ app.rb
@@ -11,19 +11,20 @@ configure :production do
   require 'newrelic_rpm'
 end
 
-$logger = Logger.new(STDOUT)
-$logger.level = Logger.const_get ENV['LOG_LEVEL'] || 'DEBUG'
+LOG = Logger.new(STDOUT)
+LOG.level = Logger.const_get ENV['LOG_LEVEL'] || 'DEBUG'
+
+REDIS_URI = URI.parse(ENV['REDISTOGO_URL'] || 'localhost:6379')
+REDIS = Redis.new(:host => REDIS_URI.host, :port => REDIS_URI.port, :password => REDIS_URI.password)
+
 POST_TIME_KEY = 'POST_TIME'
-
-uri = URI.parse(ENV['REDISTOGO_URL'] || 'localhost:6379')
-$cache = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-
 INTERVAL = 60 * 60 * 2
-if ($cache.exists(POST_TIME_KEY))
-  post_time = Time.parse($cache.get(POST_TIME_KEY))
+
+if (REDIS.exists(POST_TIME_KEY))
+  post_time = Time.parse(REDIS.get(POST_TIME_KEY))
 else
   post_time = Time.now + INTERVAL
-  $cache.set(POST_TIME_KEY, post_time.strftime('%Y/%m/%d %X'))
+  REDIS.set(POST_TIME_KEY, post_time.strftime('%Y/%m/%d %X'))
 end
 
 Twitter.configure do |config|
@@ -39,29 +40,21 @@ end
 
 EM::defer do
   crawler = Crawler.new
+  crawler.logger = LOG
+
   loop do
     if post_time < Time.now
-      stories = crawler.elements.map do |element|
-        title = element.search('dt').first.content
-
-        element.search('a').map do |a|
-          content = a.content
-          url = a.attribute('href').value
-          Story.new(title, content, url)
-        end
-      end
-      stories.flatten.reverse.each do |story|
-        if (!$cache.exists(story.key))
-          $cache.set(story.key, story.to_json)
-          $logger.info story.key + ':' + story.tweet
-          Twitter.update(story.tweet)
+      crawler.stories.each do |story|
+        if (REDIS.exists(story.key))
+          LOG.info "exists key:" + story.key
         else
-          $logger.info "exists key:" + story.key
+          LOG.info story.key + ':' + story.tweet
+          REDIS.set(story.key, story.to_json)
+          Twitter.update(story.tweet)
         end
       end
-
       post_time = Time.now + INTERVAL
-      $cache.set(POST_TIME_KEY, post_time.strftime('%Y/%m/%d %X'))
+      REDIS.set(POST_TIME_KEY, post_time.strftime('%Y/%m/%d %X'))
     end
 
     sleep(5)
--- crawler.rb
+++ crawler.rb
@@ -7,13 +7,30 @@ class Crawler
     @mechanize = Mechanize.new
   end
 
+  def logger=(logger)
+    @logger = logger
+  end
+
   def elements
     begin
       page = @mechanize.get(YUKARIN_URL)
     rescue
-      $logger.error $!
+      @logger.error $! unless @logger.nil?
     end
     return [] if page.nil?
     page.search('#news > dl')
+  end
+
+  def stories
+    results = elements.reverse.map do |element|
+      title = element.search('dt').first.content
+
+      element.search('a').map do |a|
+        content = a.content
+        url = a.attribute('href').value
+        Story.new(title, content, url)
+      end
+    end
+    results.flatten
   end
 end
